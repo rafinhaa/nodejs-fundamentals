08dbcfeb310be8c82572539089963f49d5682e4e
Rafinhaa <rafinhaa@me.com>
2023-01-27 10:52:44 -0300

    Estrutura da aplicação - Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
+import { randomUUID } from "node:crypto";
 import http from "node:http";
+
+const users = [];
 
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === "GET" && url === "/users")
-    return res.end("Listagem de usuários");
+    return res
+      .setHeader("Content-Type", "application/json")
+      .end(JSON.stringify(users));
 
-  if (method === "POST" && url === "/users")
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: randomUUID(),
+      name: "John Doe",
+      email: "johndoe@example.com",
+    });
     return res.end("Criação de usuários");
+  }
 
   return res.end("Hello World!");
 });
