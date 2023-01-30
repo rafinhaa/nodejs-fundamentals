df83d55ec8baf99295133a6aa8d6b839cc71b97f
Rafinhaa <rafinhaa@me.com>
2023-01-30 16:07:20 -0300

    Banco de dados - Persistindo banco de dados

--- src/database.js
+++ src/database.js
@@ -1,5 +1,19 @@
+import fs from "node:fs/promises";
+
+const databasePath = new URL("../db.json", import.meta.url);
+
 export class Database {
   #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, "utf8")
+      .then((data) => (this.#database = JSON.parse(data)))
+      .catch(() => this.#persist());
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
 
   select(table) {
     return this.#database[table] ?? [];
@@ -8,6 +22,8 @@ export class Database {
   insert(table, data) {
     if (Array.isArray(this.#database[table])) this.#database[table].push(data);
     else this.#database[table] = [data];
+
+    this.#persist();
 
     return data;
   }
